ed5fbb0e4f7f0eef14c5d74f1719cf6ad28f3466
@robinlant <mtarasovets08@gmail.com>
2024-03-20 03:52:16 +0100

    finish lab1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,9 @@
-﻿ using SampleDataGenerator;
+﻿using ConsoleApp;
+using FormulaCalculator.Implementations.laboratory_1;
+using SampleDataGenerator;
 
- var sampleData = DataSerializer.Deserialize<SampleData>("../../../Data/data.json");
+var sampleData = DataSerializer.Deserialize<SampleData>("../../../Data/data.json");
+var calc = new FormulaCalculatorLabOne(sampleData!);
 
-Console.Write(sampleData);
+Printer.PrintResults(calc.CalcFormulaA(),10,"Formula A Lab 1");
+Printer.PrintResults(calc.CalcFormulaB(),10,"Formula B Lab 1");
