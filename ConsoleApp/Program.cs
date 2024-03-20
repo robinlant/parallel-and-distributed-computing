92f4c513b7bef588c1aa2052a0c2f2d0255297a1
@robinlant <mtarasovets08@gmail.com>
2024-03-20 10:58:31 +0100

    litte changes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,5 +5,5 @@ using SampleDataGenerator;
 var sampleData = DataSerializer.Deserialize<SampleData>("../../../Data/data.json");
 var calc = new FormulaCalculatorLabOne(sampleData!);
 
-Printer.PrintResults(calc.CalcFormulaA(),10,"Formula A Lab 1");
-Printer.PrintResults(calc.CalcFormulaB(),10,"Formula B Lab 1");
+Printer.PrintResults(calc.CalcFormulaA(),5,"Formula A Lab 1");
+Printer.PrintResults(calc.CalcFormulaB(),5,"Formula B Lab 1");
